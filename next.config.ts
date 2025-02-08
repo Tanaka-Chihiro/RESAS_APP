66f3d80d24310a13277d25468870054f58f7d651
Tanaka Chihiro <tokyo.9st@gmail.com>
2025-02-08 15:50:21 +0900

    [add]responseヘッダーの追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
+/**@type {import('next').NextConfig} */
+const nextConfig = {
+  // 全ての API routes にマッチ
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            // CORSを許可するオリジン
+            key: "Access-Control-Allow-Origin",
 
-const nextConfig: NextConfig = {
-  /* config options here */
+            value: "https://resas-lo2o9h9n5-tanaka-chihros-projects.vercel.app",
+          },
+          {
+            // 許可するメソッド
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,POST",
+          },
+          {
+            // 許可するリクエストヘッダ
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
